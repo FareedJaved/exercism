55fd3a76565e280e62414301bcc47adbb731b0bc
Fareed Javed <fareedjaved@gmail.com>
2016-12-20 21:48:34 -0500

    Hello World

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -1,19 +1,21 @@
-//
-// This is a stub file for the 'Hello World' exercise. It's been provided as a
+
+// This is a stub file for the 'Hello World' exercise.
+// It's been provided as a
 // convenience to get you started writing code faster.
-// Make sure to look at hello-world.spec.js--that should give you some hints about what is
+// Make sure to look at hello-world.spec.js--that should
+// give you some hints about what is
 // expected here.
 
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-	// Checking if the input is an empty string 
-	if (input.length > 1) {
-		return 'Hello, ' + input + '!'; 
-	} 
-	else {
-		return 'Hello, World!'; 
-	} 
+    // Checking if the input is an empty string 
+    if (input.length < 1) {
+	return "Hello, World!"; 
+    }
+    else {
+	return "Hello, " + input + "!"; 
+    }
 };
 
 module.exports = HelloWorld;
